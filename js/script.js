e49ab3ad591591bce253d328f95716b453845511
parisdanstoutsesetats <49198909+parisdanstoutsesetats@users.noreply.github.com>
2019-04-03 21:42:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+ /*
 var map = document.querySelector('#map')
 var paths = map.querySelectorAll('.map__image polygon')
 var lists = map.querySelectorAll('.map__list li')
@@ -9,8 +10,38 @@ paths.forEach(function(path){
             item.classList.remove('is-active')
         })
         document.querySelector('#list_'+id).classList.add("is-active")
-        document.querySelector(id).classList.add("is-active")
-
     })
     
 })
+*/
+
+function displayName(name) {
+    document.getElementById('quartier').firstChild.data = name;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World"}
+
+var data1 = [['75001', '75002','75008'],
+             ['75003', '75009', '75010'],
+             ['75012', '75004','75006'],
+             ['75005', '75013', '75020','75011'],
+             ['75015', '75017', '75018'],
+             ['75019', '75016','75007','75014']]
+             ;
+
+function colourCountry(name, colour) {
+    var country = document.getElementById(name);
+    document.getElementById(name).classList.add('colour' + colour);
+}
+
+
+
+function colourCountries(data) {
+  for (var colour = 0; colour < data.length; colour++){    
+    for (var country = 0; country < data[colour].length; country++){
+      colourCountry(data[colour][country], colour + 1);
+    }
+  }
+}
+colourCountries(data1)
