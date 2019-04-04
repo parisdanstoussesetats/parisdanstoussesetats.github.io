c8829a543ddf64e38b8542eb186db44a09319812
parisdanstoutsesetats <49198909+parisdanstoutsesetats@users.noreply.github.com>
2019-04-04 14:43:36 +0200

    Update pie.js

--- js/pie.js
+++ js/pie.js
@@ -1,1 +1,41 @@
+var piewidth = 600,
+    pieheight = 600,
+    radius = Math.min(piewidth, pieheight) / 2;
 
+color = d3.scale.category10();
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.total; });
+
+var piesvg = d3.select("body").append("svg")
+    .attr("width", piewidth)
+    .attr("height", pieheight)
+  .append("g")
+    .attr("transform", "translate(" + piewidth / 2 + "," + pieheight / 2 + ")");
+
+d3.csv("music.csv", function(error, data) {
+    data.forEach(function(d) {
+    d.total = +d.total;
+});
+
+    var g = piesvg.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+    .attr("class", "arc");
+
+    g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data.style); });
+
+    g.append("text")
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function(d) { return d.data.style; });
+
+});
