63e6c4e70812aaead13044a166bd5c32d20817a9
parisdanstoutsesetats <49198909+parisdanstoutsesetats@users.noreply.github.com>
2019-04-04 16:09:57 +0200

    Update pie.js

--- js/pie.js
+++ js/pie.js
@@ -1,42 +1,79 @@
-var piewidth = 600,
-    pieheight = 600,
-    radius = Math.min(piewidth, pieheight) / 2;
 
-color = d3.scaleBand()
-    .range(["#CFF09E", "#A8DBA8", "#79BD9A", "#3B8686", "#0B486B", "#588A70"]);
-
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.total; });
-
-var piesvg = d3.select("body").append("svg")
-    .attr("width", piewidth)
-    .attr("height", pieheight)
-  .append("g")
-    .attr("transform", "translate(" + piewidth / 2 + "," + pieheight / 2 + ")");
-
-d3.csv("../data/music.csv", function(error, data) {
-    data.forEach(function(d) {
-    d.total = +d.total;
-});
-
-    var g = piesvg.selectAll(".arc")
-    .data(pie(data))
-    .enter().append("g")
-    .attr("class", "arc");
-
-    g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color(d.data.style); });
-
-    g.append("text")
-    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-    .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .text(function(d) { return d.data.style; });
-
-});
+        var svg = d3.select("body").append("svg")
+          .attr("width", 960)
+          .attr("height", 500)
+          .append("g");
+        
+        svg.append("g")
+                .attr("class", "slices");
+        svg.append("g")
+          .attr("class", "labels");
+        svg.append("g")
+          .attr("class", "lines");
+        var width = 500;
+        var height = 400;
+        var radius = Math.min(width, height)/2;
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
+        var data = [26,25,10,30,30,50,34];
+        var labels = ['rock','electro','jazz','pop','variété','classique','tropical']
+        
+        
+        
+        var pie = d3.pie().sort(null).value(d => d);
+        var arc = d3.arc().innerRadius(radius*0.8).outerRadius(radius*0.6);
+        
+         var outerArc = d3.arc()
+                    .outerRadius(radius * 0.9)
+                    .innerRadius(radius * 0.9);
+         
+        svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        
+        svg.selectAll('path')
+        .data(pie(data))
+        .enter()
+        .append('path')
+          .attr('d', arc)
+        .attr('fill', (d,i)=> color(i));
+        svg.append('g').classed('labels',true);
+        svg.append('g').classed('lines',true);
+         
+    
+         var polyline = svg.select('.lines')
+                    .selectAll('polyline')
+                    .data(pie(data))
+                  .enter().append('polyline')
+                    .attr('points', function(d) {
+      
+                        // see label transform function for explanations of these three lines.
+                        var pos = outerArc.centroid(d);
+                        pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+                        return [arc.centroid(d), outerArc.centroid(d), pos]
+                    });
+            
+            
+        
+           var label = svg.select('.labels').selectAll('text')
+                    .data(pie(data))
+                  .enter().append('text')
+                    .attr('dy', '.35em')
+                    .html(function(d) {
+                        return d.data;
+                    })
+                    .attr('transform', function(d) {
+                        var pos = outerArc.centroid(d);
+                        pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+                        return 'translate(' + pos + ')';
+                    })
+                    .style('text-anchor', function(d) {
+                        return (midAngle(d)) < Math.PI ? 'start' : 'end';
+                    });
+        
+         svg.append('text')
+                            .attr('class', 'toolCircle')
+                            .attr('dy', -15) // hard-coded. can adjust this to adjust text vertical alignment in tooltip
+                            .html('Style musical des parisiens') // add text to the circle.
+                            .style('font-size', '.9em')
+                            .style('text-anchor', 'middle');
+        
+        function midAngle(d) { return d.startAngle + (d.endAngle - d.startAngle) / 2; } 
+        
