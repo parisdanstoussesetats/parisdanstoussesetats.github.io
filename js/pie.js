32b6d9ee0ce4a207bc72b23a81169811552bca15
parisdanstoutsesetats <49198909+parisdanstoutsesetats@users.noreply.github.com>
2019-04-04 15:07:57 +0200

    Update pie.js

--- js/pie.js
+++ js/pie.js
@@ -2,7 +2,8 @@ var piewidth = 600,
     pieheight = 600,
     radius = Math.min(piewidth, pieheight) / 2;
 
-color = d3.scale.category10();
+color = d3.scale.ordinal()
+    .range(["#CFF09E", "#A8DBA8", "#79BD9A", "#3B8686", "#0B486B", "#588A70"]);
 
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
